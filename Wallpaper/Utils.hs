718baefb0604c708b448b4bdb0c68864e75430c1
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-01-21 18:38:42 +0100

    first commit

---
+++ Wallpaper/Utils.hs
@@ -0,0 +1,32 @@
+module Wallpaper.Utils (
+    countFilesR
+    ) where
+
+import           Control.Monad    (filterM, liftM)
+import           Data.List        (isSuffixOf, partition)
+
+import           Pipes            (Producer (..), each, lift)
+
+import           Pipes.Prelude    (toListM)
+import           System.Directory (doesDirectoryExist, doesFileExist,
+                                   getDirectoryContents)
+import           System.FilePath  ((</>))
+
+dirRWalker :: FilePath -> Producer FilePath IO ()
+dirRWalker root = do
+    cts <- lift $ getDirectoryContents root
+    let paths = map (root </>) cts
+    files <- filterM (lift . doesFileExist) paths
+    dirs <- filterM (lift . realDir) paths
+    each files
+    mapM_ dirRWalker dirs
+    where
+        realDir x | ".." `isSuffixOf` x = return False
+        realDir x | "." `isSuffixOf` x = return False
+        realDir x = doesDirectoryExist x
+
+countFilesR :: FilePath -> IO Int
+countFilesR root = do
+    files <- toListM $ dirRWalker root
+    return $ length files
+
---
+++ Wallpaper/Wallpaper.hs
@@ -0,0 +1,58 @@
+module Wallpaper.Wallpaper (
+          Config(..)
+        , wallpaper
+    ) where
+
+import           System.IO                       (writeFile)
+import           System.Process                  (callCommand)
+import           Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import           Text.Blaze.Html5                (Html (..))
+import           Wallpaper.Utils
+
+
+
+data Config d = Config {
+      generator :: d -> Html
+    , parser    :: IO d
+    }
+
+wallpaperFileName :: String
+wallpaperFileName = "wallpaper"
+
+generateImage :: FilePath -> FilePath -> IO ()
+generateImage html png = callCommand generateCmd
+    where
+        generateCmd = unwords $ generateBin :generateFlags ++ [generateInputHtml, generateOutputPng]
+        generateBin = "wkhtmltoimage"
+        generateFlags = ["--width", width, "--height", height]
+        generateInputHtml = html
+        generateOutputPng = png
+        width = "1920"
+        height = "1080"
+
+putUpWallpaper :: FilePath -> IO ()
+putUpWallpaper png = callCommand putCmd
+    where
+        putCmd = unwords $ putBin: putFlags
+        putBin = "feh"
+        putFlags = ["--bg-fill", putInputPng]
+        putInputPng = png
+
+wallpaper :: Config a -> IO ()
+wallpaper c = do
+
+    -- generate html
+    dat <- parser c
+    let html = renderHtml $ (generator c) dat
+    writeFile wallpaperHtmlFile html
+
+    -- Generate wallpaper
+    generateImage wallpaperHtmlFile wallpaperPngFile
+
+    -- Put up wallpaper
+    putUpWallpaper wallpaperPngFile
+
+    where
+        wallpaperHtmlFile = wallpaperFileName ++ ".html"
+        wallpaperPngFile  = wallpaperFileName ++ ".png"
+
